9e37b5b822561c0062b71d4b658f1c823350b22f
Marco Klein <hobart2967@outlook.com>
2023-09-30 00:57:08 +0200

    fix build

--- .releaserc.js
+++ .releaserc.js
@@ -16,7 +16,7 @@ module.exports = {
       }
     ],
     "@semantic-release/release-notes-generator",
-    "@semantic-release/github",
+    //"@semantic-release/github",
     ["@semantic-release/git", {
       "assets": ["package.json"],
       "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
